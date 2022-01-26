4a7db6edff767110045224090a7d9af8cfaba9b2
michael-kip <michaelkiprotich@student.moringaschool.com>
2022-01-26 06:58:22 +0300

    adding route to t=retreive users form input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,10 +8,18 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        get("/form", ((request, response) -> {
+        get("/form", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
 
             return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/quote_list", (request, response) -> {
+            Map<String, Object>model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String description = request.queryParams("name");
+            model.put("name","name");
+            model.put("description", "description");
+            return new ModelAndView(model, "quote_list.hbs");
         }, new HandlebarsTemplateEngine());
 
     }
